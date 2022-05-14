7d61a4844f719cafd42668947ea494b9c04e6c03
SiddhanthDubey <91405555+SiddhanthDubey@users.noreply.github.com>
2022-05-14 13:11:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,12 @@
-<!DOCTYPE html>
 <html>
     <head>
-
-        <link rel="stylesheet" type="text/css" href="../css/ClassesAndID.css">
-        <title>Classes And ID</title>
-
     </head>
     <body>
-        <h1 class="t">My Website</h1>
-        <p>Lorem ipsum dolor sit aet, consectetur adipisicing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.</p>
-        <h3 id="h3" class="subtitle">About Me</h3>
-        <img src="../img/scenery.jpg" alt="Scenery" width="100%">
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qus nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <p>Cillum dolore eu fugiat nulla, pariatur:</p>
-
-        <ul>
-            <li>Excepture sint occaecat</li>
-            <li>cupidatat non proident</li>
-            <li>sunt in culpa</li>
-        </ul>
-
-        <h2>Services</h2>
-
-        <table border="2">
-            <thead>
-                <tr>
-                    <td>Lorem</td>
-                    <td>Ipsum</td>
-                    <td>Dolor</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Sit</td>
-                    <td>Amet</td>
-                    <td>Consectetur</td>
-                </tr>
-                <tr>
-                    <td>Adipisicing</td>
-                    <td>Elit</td>
-                    <td>Sed</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <img src="../img/tree.jpg" alt="Island with three coconut trees">
-        <h2 id="contact-us">Contact Us</h2>
-        <form action="">
-            <input type="text" placeholder="Full Name">
-            <input type="email" placeholder="Your Email">
-            <input type="number" placeholder="Your Number">
+        <form action="gradesprinter.php" method="post">
+        Enter Your Name To Get Your Marks: <input type="text" name = "name">
+        <br>
+        <input type="submit">
         </form>
+    <?php?>
     </body>
 </html>
